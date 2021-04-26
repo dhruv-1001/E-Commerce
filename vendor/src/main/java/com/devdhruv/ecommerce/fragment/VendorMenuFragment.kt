027a9b26befc0b76c939b9a7fafe267310f6c2cc
dhruvbaliyan <dhruvbaliyan1001@gmail.com>
2021-04-26 14:51:31 +0530

    Added intent to get images from device to menu fragment image view

--- vendor/src/main/java/com/devdhruv/ecommerce/fragment/VendorMenuFragment.kt
+++ vendor/src/main/java/com/devdhruv/ecommerce/fragment/VendorMenuFragment.kt
@@ -1,9 +1,13 @@
 package com.devdhruv.ecommerce.fragment
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -11,9 +15,17 @@ import com.devdhruv.ecommerce.R
 import com.devdhruv.ecommerce.databinding.FragmentVendorMenuBinding
 import com.devdhruv.ecommerce.viewModel.VendorMenuViewModel
 import com.devdhruv.ecommerce.viewModel.VendorMenuViewModelFactory
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
 
 class VendorMenuFragment : Fragment() {
 
+    lateinit var imageUri: Uri
+    var selectedImage = 1
+    lateinit var firebseStorage: FirebaseStorage
+    lateinit var storageReference: StorageReference
+    lateinit var viewModel: VendorMenuViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,15 +36,45 @@ class VendorMenuFragment : Fragment() {
             inflater, R.layout.fragment_vendor_menu, container, false
         )
 
+        firebseStorage = FirebaseStorage.getInstance()
+        storageReference = firebseStorage.reference
+
         val viewModelFactory = VendorMenuViewModelFactory()
-        val viewModel = ViewModelProvider(this, viewModelFactory).get(VendorMenuViewModel::class.java)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(VendorMenuViewModel::class.java)
 
         binding.vendorMenuViewModel = viewModel
         binding.lifecycleOwner = this
 
-
-
+        viewModel.imageClicked.observe(viewLifecycleOwner, {
+            if (it != null) {
+                selectedImage = it
+            }
+            choosePicture()
+        })
         return binding.root
+    }
+
+    private fun choosePicture(){
+
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, 1)
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null){
+            imageUri = data.data!!
+            viewModel.changeImage(imageUri, selectedImage)
+            when (selectedImage) {
+                1 -> view?.findViewById<ImageView>(R.id.ivProductImage1)?.setImageURI(imageUri)
+                2 -> view?.findViewById<ImageView>(R.id.ivProductImage2)?.setImageURI(imageUri)
+                else -> view?.findViewById<ImageView>(R.id.ivProductImage3)?.setImageURI(imageUri)
+            }
+        }
     }
 
 }
--- vendor/src/main/java/com/devdhruv/ecommerce/viewModel/VendorMenuViewModel.kt
+++ vendor/src/main/java/com/devdhruv/ecommerce/viewModel/VendorMenuViewModel.kt
@@ -1,9 +1,36 @@
 package com.devdhruv.ecommerce.viewModel
 
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.devdhruv.ecommerce.R
 
 class VendorMenuViewModel: ViewModel() {
 
+    private var _imageClicked =  MutableLiveData<Int?>()
+    val imageClicked: LiveData<Int?> get() = _imageClicked
 
+    var productOneImage: Uri = Uri.parse("android.resource://com.devdhruv.ecommerce/" + R.drawable.background_image)
+
+    var productTwoImage: Uri = Uri.parse("android.resource://com.devdhruv.ecommerce/" + R.drawable.background_image)
+
+    var productThreeImage: Uri = Uri.parse("android.resource://com.devdhruv.ecommerce/" + R.drawable.background_image)
+
+    fun changeImage(imageUri: Uri, selectedImage: Int) {
+        when (selectedImage){
+            1 -> productOneImage = imageUri
+            2 -> productTwoImage = imageUri
+            else -> productThreeImage = imageUri
+        }
+    }
+
+    fun onImageUploadClick(index: Int){
+        _imageClicked.value = index
+    }
+
+    fun onClickUpload(){
+
+    }
 
 }
