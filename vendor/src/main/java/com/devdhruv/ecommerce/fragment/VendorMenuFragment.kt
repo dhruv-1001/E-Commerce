3f84ac5e4070ce6a5da62de305e7ac0775e10bb3
dhruvbaliyan <dhruvbaliyan1001@gmail.com>
2021-04-29 08:58:58 +0530

    Functionality added to edit text

--- vendor/src/main/java/com/devdhruv/ecommerce/fragment/VendorMenuFragment.kt
+++ vendor/src/main/java/com/devdhruv/ecommerce/fragment/VendorMenuFragment.kt
@@ -2,12 +2,11 @@ package com.devdhruv.ecommerce.fragment
 
 import android.app.Activity.RESULT_OK
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -15,6 +14,7 @@ import com.devdhruv.ecommerce.R
 import com.devdhruv.ecommerce.databinding.FragmentVendorMenuBinding
 import com.devdhruv.ecommerce.viewModel.VendorMenuViewModel
 import com.devdhruv.ecommerce.viewModel.VendorMenuViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
@@ -22,8 +22,9 @@ import com.google.firebase.storage.StorageReference
 class VendorMenuFragment : Fragment() {
 
     var selectedImage = 1
-    lateinit var firebaseStorage: FirebaseStorage
-    lateinit var storageReference: StorageReference
+    var firebaseStorage: FirebaseStorage = FirebaseStorage.getInstance()
+    var storageReference: StorageReference = firebaseStorage.reference
+
     lateinit var viewModel: VendorMenuViewModel
 
     override fun onCreateView(
@@ -34,9 +35,6 @@ class VendorMenuFragment : Fragment() {
         val binding: FragmentVendorMenuBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_vendor_menu, container, false
         )
-
-        firebaseStorage = FirebaseStorage.getInstance()
-        storageReference = firebaseStorage.reference
 
         val viewModelFactory = VendorMenuViewModelFactory(firebaseStorage, storageReference)
         viewModel = ViewModelProvider(this, viewModelFactory).get(VendorMenuViewModel::class.java)
@@ -50,6 +48,11 @@ class VendorMenuFragment : Fragment() {
             }
             choosePicture()
         })
+
+        viewModel.blankFields.observe(viewLifecycleOwner, {
+            Snackbar.make(requireView(), "Enter All The Fields", Snackbar.LENGTH_SHORT).show()
+        })
+
         return binding.root
     }
 
--- vendor/src/main/java/com/devdhruv/ecommerce/viewModel/VendorMenuViewModel.kt
+++ vendor/src/main/java/com/devdhruv/ecommerce/viewModel/VendorMenuViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.devdhruv.ecommerce.R
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
@@ -16,11 +17,21 @@ class VendorMenuViewModel(
     private var _imageClicked =  MutableLiveData<Int?>()
     val imageClicked: LiveData<Int?> get() = _imageClicked
 
+    private var _blankFields = MutableLiveData<Boolean>()
+    val blankFields: LiveData<Boolean?>  get() = _blankFields
+
     private val defaultUri: Uri = Uri.parse("android.resource://com.devdhruv.ecommerce/" + R.drawable.background_image)
 
     var productOneUri = MutableLiveData(defaultUri)
     var productTwoUri = MutableLiveData(defaultUri)
     var productThreeUri = MutableLiveData(defaultUri)
+
+    var category = MutableLiveData<String>()
+    var productName = MutableLiveData<String>()
+    var priceAmt = MutableLiveData<String>()
+    var gstAmt = MutableLiveData<String>()
+    var deliveryCharge = MutableLiveData<String>()
+    var offer = MutableLiveData<String>()
 
     fun changeImage(imageUri: Uri, selectedImage: Int) {
         when (selectedImage){
@@ -36,12 +47,31 @@ class VendorMenuViewModel(
 
 
     private fun checkFields(): Boolean{
+
+        if (productOneUri.value == defaultUri
+            || productTwoUri.value == defaultUri
+            || productThreeUri.value == defaultUri
+            || category.value.isNullOrBlank()
+            || productName.value.isNullOrBlank()
+            || priceAmt.value.isNullOrBlank()
+            || gstAmt.value.isNullOrBlank()
+            || deliveryCharge.value.isNullOrBlank()
+            || offer.value.isNullOrBlank()){
+            return false
+        }
         return true
+    }
+
+    private fun updateBlankFields(){
+        _blankFields.value = _blankFields.value != true
     }
 
     fun onClickUpload(){
         if (checkFields()){
 
+        }
+        else{
+            updateBlankFields()
         }
     }
 
